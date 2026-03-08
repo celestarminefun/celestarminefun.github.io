854ec3b22efa648ab6bc1ebbacdc22717927cbcb
celestarminefun <douglashaha1207@gmail.com>
2026-03-08 11:32:30 +0800

    Create keystrokes.user.js

---
+++ mods/installation/keystrokes.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Keystrokes Mod | Celestar
+// @namespace    celestarminefun.github.io
+// @version      1.0
+// @match        *://minefun.io/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const script = document.createElement('script');
+    script.src = 'https://celestarminefun.github.io/mods/keystrokes.user.js';
+    script.type = 'text/javascript';
+    script.async = true;
+
+    document.head.appendChild(script);
+})();
