2db8058ab7a41c24409be8c422392adc7954c1a9
celestarminefun <douglashaha1207@gmail.com>
2026-09-06 16:48:43 +0800

    Create celestar-mod-menu-v2.user.js

---
+++ celestar-mod-menu-v2.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Celestar Mod Menu v2
+// @namespace    https://celestarminefun.github.io
+// @version      2.0.0
+// @description  Most advanced and feature-rich mod menu for MineFun.io
+// @author       Celestar / thetalkingcat (@thetalkingcat8089)
+// @match        https://minefun.io/*
+// @match        https://sandbox.minefun.io/*
+// @run-at       document-start
+// @require      https://celestarminefun.github.io/celestar-mod-menu-v2.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @connect      celestarminefun.github.io
+// @connect      minefun.io
+// @connect      sandbox.minefun.io
